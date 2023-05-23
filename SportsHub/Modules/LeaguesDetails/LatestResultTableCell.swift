1d97837a9b03a5d5839ee0fc9e4de96fed321a0a
YousraMamdouh <yousramamdouh98@gmail.com>
2023-05-23 13:12:57 -0700

    modules

---
+++ SportsHub/Modules/LeaguesDetails/LatestResultTableCell.swift
@@ -0,0 +1,23 @@
+//
+//  LatestResultTableCell.swift
+//  SportsHub
+//
+//  Created by Yousra Mamdouh Ali on 23/05/2023.
+//
+
+import UIKit
+
+class LatestResultTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
